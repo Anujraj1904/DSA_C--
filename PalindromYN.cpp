7917dabb31494d8657ab1fe068c0d65260f55a5d
Anuj Raj <akgupta.anuj@gmail.com>
2025-09-04 16:33:43 +0530

    string problem

---
+++ PalindromYN.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isPalindrome(string str)
+{
+  int st = 0;
+  int end = str.length() - 1;
+
+  while (st < end)
+  {
+    if (str[st] != str[end])
+    {
+      return false;
+    }
+    st++;
+    end--;
+  }
+
+  return true;
+}
+
+int main()
+{
+  string str;
+  cout << "Enter you word to if PALINDROM or NOT : ";
+  getline(cin, str);
+
+  cout << "String : " << str << endl;
+
+
+
+  if (isPalindrome(str))
+  {
+    cout << "It's a palindrome!" << endl;
+  }
+  else
+  {
+    cout << "Not a palindrome." << endl;
+  }
+  return 0;
+}
