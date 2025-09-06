e175b7fcf0da57107f9208dc206e4bf2e35c0926
Anuj Raj <akgupta.anuj@gmail.com>
2025-09-07 02:31:51 +0530

    maxRowSum

---
+++ 2dArray_Matrix/maxRowSum.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int getMaxSum(int matrix[4][3], int rows, int cols){
+  int maxRowSum = INT_MIN;
+
+  for(int i =0; i<rows; i++){
+    int rowSumI =0;
+    for(int j = 0; j<cols; j++){
+      rowSumI += matrix[i][j];
+    }
+
+    maxRowSum = max(maxRowSum, rowSumI);
+  }
+
+return maxRowSum;
+}
+
+int main(){
+  int matrix[4][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
+  int rows = 4;
+  int cols = 3;
+
+  cout << getMaxSum(matrix, rows, cols) << endl;
+  return 0;
+}
