d3daef4b27f264f6b8c064f81b8fd8e3985f42d0
Anuj Raj <akgupta.anuj@gmail.com>
2025-09-23 02:56:04 +0530

    UPDATE

---
+++ Conditionals(02)/BasicSyntax.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+  // if(condition){
+  //   cout << "Statement";
+  // }else {
+  //   cout << "Statement";
+  // }
+
+  int a = 5;
+  int b = 7;
+
+  if(a>b){
+    cout << a <<" is greater than " << b << '\n';
+  } else {
+    cout << a << " is smaller than " << b << '\n';
+  }
+  return 0;
+}
---
+++ Conditionals(02)/oddEven.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num;
+  cout << "Enter a number : ";
+  cin >> num;
+
+  if (num % 2 == 0) {
+    cout << num <<  " is a Even number" << "\n";
+  }else {
+    cout << num << " is a Odd number" << "\n";
+  }
+  return 0;
+}
